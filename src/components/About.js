6ce8d06ea632969e2d45d0889a6827bf13e1c408
Thazar-r <melchior.balthazar@student.moringaschool.com>
2024-07-31 13:53:22 +0300

    complete labs

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+<h2>About Me</h2>
+      <p>
+        I am passionate about web development and enjoy creating interactive and dynamic user experiences. I am always eager to learn new technologies and improve my skills.
+      </p>
+      <img src={image} alt="I made this" />
+  </div>;
 }
 
 export default About;
